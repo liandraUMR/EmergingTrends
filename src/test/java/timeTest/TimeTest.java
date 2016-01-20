5077b2e1e69db559f69a5b35393c4ee73976ddc4
631152 <b.martinez.95@hotmail.com>
2016-01-19 22:08:35 -0700

    commit numero uno

---
+++ src/test/java/timeTest/TimeTest.java
@@ -0,0 +1,22 @@
+package timeTest;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by 631152 on 1/19/2016.
+ */
+public class TimeTest
+{
+
+    @Before
+    public void setup()
+    {
+
+    }
+    @Test
+    public void test1()
+    {
+        System.out.println("test");
+    }
+}
