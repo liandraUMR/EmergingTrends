0263637b2a0aa465336a27344ce5d3088dd09e0c
631152 <b.martinez.95@hotmail.com>
2016-01-19 22:10:45 -0700

    commit numero uno

---
+++ src/main/java/emergingTrends/exceptions/InvalidTimeException.java
@@ -0,0 +1,12 @@
+package emergingTrends.exceptions;
+
+/**
+ * Created by 631152 on 1/19/2016.
+ */
+public class InvalidTimeException extends Exception
+{
+    public InvalidTimeException(String msg)
+    {
+        super(msg);
+    }
+}
